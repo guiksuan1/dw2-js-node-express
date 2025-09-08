638f62794988a60d7b013f5b4a6735d82a265716
guiksuan <guilhermekauansb@gmail.com>
2025-09-08 19:43:58 -0300

    aa

---
+++ JavaScript/02.1/js-02.01.js
@@ -0,0 +1,11 @@
+const products = ["Computador", "Notebook", "Celular", "Tablet"]
+document.write(`<p>${products}</p>`)
+document.write(typeof(products))
+document.write(`<p>${products[1]}</p>`)
+document.write(`<p>Exibindo um elemnto do Array atráves do índice </p>`)
+document.write(`<p>${products[3]}</p>`)
+
+document.write(`<p>Percorrendo </p>`)
+products.forEach(function (produto) {
+    document.write(`<p>${produto}</p>`)
+})
